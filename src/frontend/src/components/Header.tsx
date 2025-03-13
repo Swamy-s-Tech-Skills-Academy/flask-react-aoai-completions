216b37675131a00ce103536e4c4c79e8cb86c31b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-13 22:23:57 +0530

    Soft Colors

--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
     // const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
     return (
-        <header className="bg-gray-600 text-white h-12 flex justify-between items-center px-4 shadow-md font-inter">
+        <header className="bg-gray-200 text-Navy-800 h-12 flex justify-between items-center px-4 shadow-md font-inter">
             <h1 className="text-xl font-bold tracking-wide">Single Turn ChatGPT Clone</h1>
             {/* <button
                 className="bg-white text-blue-600 px-4 py-1 rounded transition duration-200 hover:bg-gray-200 active:scale-95"
